78d25be0c65153a775d6d3b35143fa65dd78c0c4
Kevin Fronczak <kfronczak@gmail.com>
2017-09-09 17:12:08 -0400

    Added config panel in, smash script combines dirs to be compatible

--- bin/smash.sh
+++ bin/smash.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 python3 smash.py --source automation/ --dest automations.yaml --ext yaml --use-ids --force-overwrite
 python3 smash.py --source scripts/ --dest scripts.yaml --ext yaml --force-overwrite
+python3 smash.py --source customize/ --dest customize.yaml --ext yaml --force-overwrite
+sudo chown hass /home/hass/.homeassistant/automations.yaml
+sudo chown hass /home/hass/.homeassistant/scripts.yaml
+sudo chown hass /home/hass/.homeassistant/customize.yaml
