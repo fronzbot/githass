5b24e5f105d70dfae288258cf2b2a347523ae2b0
Kevin Fronczak <kfronczak@gmail.com>
2018-05-28 17:22:26 -0400

    Set cool/heat rather than auto
    - Also added ecobee configurator to frontend, just in case
    - Removed max/min from darksky due to 0.70 change
    - Added ecobee weather component to frontend

--- python_scripts/thermostat_engine.py
+++ python_scripts/thermostat_engine.py
@@ -14,15 +14,18 @@ SLEEP_TIME = [5, 21]
 temp2 = hass.states.get('sensor.pws_temp_f').state
 temp1 = hass.states.get('sensor.dark_sky_temperature').state
 
-if temp1 is None:
-    outside_temp = float(temp2)
-else:
+try:
     outside_temp = float(temp1)
+except TypeError:
+    outside_temp = float(temp2)
+
 living_room_temp = float(hass.states.get('sensor.living_room_temperature').state)
+
 try:
     bedroom_temp = float(hass.states.get('sensor.bedroom_temperature').state)
 except ValueError:
     bedroom_temp = living_room_temp
+
 living_room_humidity = float(hass.states.get('sensor.living_room_humidity').state)
 
 # Get various system stats
@@ -44,17 +47,20 @@ else:
 too_humid = living_room_humidity > 59
 too_hot_inside = (outside_temp > 74 and (living_room_temp >= (outside_temp + 1)))
 
+logger.warning("Outside: {}, Living Room: {}, Home: {}, Time: {}, State: {}".format(outside_temp, living_room_temp, someone_home, current_time, state_key))
+
 # Only fire if thermostat is enabled
 if thermostat_enable:
     target_high = 82
     target_low = 58
+    nominal_temp = 70
     mode = 'off'
     if outside_temp > THRESHOLD_FOR_AC:
-        mode = 'auto'
+        mode = 'cool'
         target_high = AC[state_key]
         nominal_temp = AC[state_key]
     elif outside_temp < THRESHOLD_FOR_HEAT:
-        mode = 'auto'
+        mode = 'heat'
         target_low = HEAT[state_key]
         nominal_temp = HEAT[state_key]
 #    elif state_key != 'sleep' and (too_hot_inside or too_humid):
@@ -63,11 +69,13 @@ if thermostat_enable:
 #        nominal_temp = living_room_temp - 1
     # Now make service call
 
-    logger.info('Mode: {}, Outside: {}, Target High: {}, Target Low: {}'.format(mode, outside_temp, target_high, target_low))
+    logger.warning('Mode: {}, Outside: {}, Temperature: {}'.format(mode, outside_temp, nominal_temp))
     data_mode = {'entity_id': 'climate.living_room', 'operation_mode': mode}
     hass.services.call('climate', 'set_operation_mode', data_mode)
     if mode != 'off':
-        data_temps = {'entity_id': 'climate.living_room', 'temperature': nominal_temp, 'target_temp_high': target_high, 'target_temp_low': target_low}
+        time.sleep(0.5)
+        data_temps = {'entity_id': 'climate.living_room', 'temperature': nominal_temp}
         hass.services.call('climate', 'set_temperature', data_temps)
 
-    hass.services.call('input_boolean', 'turn_off', {'entity_id': 'input_boolean.on_the_way_home'})
+    if on_the_way_home:
+        hass.services.call('input_boolean', 'turn_off', {'entity_id': 'input_boolean.on_the_way_home'})
