f9dd24c7649ebdf7cfaf00678317b3b492f60fb4
Kevin Fronczak <kfronczak@gmail.com>
2018-07-07 10:13:09 -0400

    Added monitored_condition to pihole (0.73 fix)
    - Also cleaned up bin folder

--- bin/gen_ban_list.sh
+++
@@ -1,4 +0,0 @@
-more /var/log/syslog | grep fail2ban | grep WARNING > /home/hass/.homeassistant/ip_ban_list.log
-sed -i 's/dragonstone fail2ban\.actions\[[^]]*\]: WARNING//g;s/\[//g;s/\]//g;s/  / /g' /home/hass/.homeassistant/ip_ban_list.log
-python3 /home/hass/.homeassistant/bin/read_ban_list.py '/home/hass/.homeassistant/ip_ban_list.log' '/home/hass/.homeassistant/ip_ban_list.json'
-rm /home/hass/.homeassistant/ip_ban_list.log
--- bin/gitpull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ~hass/.homeassistant/
-git pull --no-edit
