e36c9833d2db5cd01bec0d7026ea76f230a97e5d
Kevin Fronczak <kfronczak@gmail.com>
2019-01-06 20:16:23 -0500

    Improved flux app

--- apps/flux.py
+++ apps/flux.py
@@ -8,15 +8,16 @@ conditions within the core (occasionally automations won't trigger
 and turning the switch off won't actually do anything... at least in
 my experience, that is)
 
-There are four segments:
+There are two color-changing segments:
 
-sunrise-offset <--> sunrise         ==> nighttime to twilight
-sunrise        <--> sunrise+offset  ==> twlight to daytime
-sunset-offset  <--> sunset          ==> daytime to twlight
-sunset         <--> sunset+offset   ==> twlight to nighttime
+sunrise --> from sunrise+offset to day+offset
+sunset --> from sunset+offset to night+offset
+
+in between sunrise and sunset, we are at day
+in between sunset and sunrise we are at night
 
 --------------------------
-appdaemon.yaml setup:
+appdaemon.yaml setup example:
 
 flux:
     module: flux
@@ -27,90 +28,111 @@ flux:
 '''
 
 import appdaemon.plugins.hass.hassapi as hass
-import datetime
+import datetime as dt
+import pytz
 
 DEBUG = False
+
 
 class Flux(hass.Hass):
 
     def initialize(self):
-        # Colors in Kelvin
-        self.sun_offset_mins = [-45, 90, -60, 120]
-        self.daytime_color   = 5000
-        self.twilight_color  = 3000
-        self.nighttime_color = 2700
-        self.perc_complete   = 0
-        self.brightness      = 254
-        self.color_dict      = {'twilight_day': self.nighttime_color, 'daytime': self.twilight_color, 'twilight_night': self.daytime_color, 'nighttime': self.twilight_color}
+        # Colors dict contains:
+        # Color temperature
+        # Starting offset in minutes
+        self.colors = {
+            'sunrise': {'temp': 3000, 'offset': -45},
+            'day': {'temp': 5000, 'offset': 30},
+            'sunset': {'temp': 3000, 'offset': -45},
+            'night': {'temp': 2700, 'offset': 60}
+        }
+        self.offsets = {}
+        for key, value in self.colors.items():
+            self.offsets[key] = dt.timedelta(minutes=value['offset'])
 
-        # Get the start time for the intital segment
-        self.current_time = self.datetime()
-        self.get_start_time()
+        self.current_time = self.time()
 
-        self.run_minutely(self.update_color, datetime.datetime.now())
+        self.run_minutely(self.update_color, self.current_time)
 
-        for light in self.split_device_list(self.args["light"]):
+        for light in self.split_device_list(self.args['light']):
             self.listen_state(self.update_on_change, light)
 
     def update_on_change(self, entity, attribute, old, new, kwargs):
         if self.get_state(entity) == "on" and old == "off":
-            #self.log(entity + " just turned on")
             self.update_color(None)
 
+    def determine_color_state(self, now):
+        sunrise = self.sunrise() - dt.timedelta(days=1)
+        sunset = self.sunset() - dt.timedelta(days=1)
+
+        self.times = {
+            'sunrise': sunrise + self.offsets['sunrise'],
+            'day': sunrise + self.offsets['day'],
+            'sunset': sunset + self.offsets['sunset'],
+            'night': sunset + self.offsets['night']
+        }
+        
+        now_time = now.time()
+
+        if now_time <= self.times['sunrise'].time():
+            return 'night'
+        elif now_time >= self.times['sunrise'].time() and now_time < self.times['day'].time():
+            return 'sunrise'
+        elif now_time >= self.times['day'].time() and now_time < self.times['sunset'].time():
+            return 'day'
+        elif now_time >= self.times['sunset'].time() and now_time < self.times['night'].time():
+            return 'sunset'
+        elif now_time >= self.times['night'].time():
+            return 'night'
+
+        # If something errored, just assume we are in night mode
+        return 'night'
+    
+    def calculate_progress(self, now, state):
+        if state == 'sunrise':
+            start = self.times['sunrise']
+            end = self.times['day']
+        elif state == 'sunset':
+            start = self.times['sunset']
+            end = self.times['night']
+        else:
+            return 1
+
+        progress = round((now - start) / (end - start), 2)
+
+        return progress
+
+    def calculate_color(self, state, progress):
+        if state == 'sunrise':
+            color_delta = self.colors['day']['temp'] - self.colors['night']['temp']
+            new_color = self.colors['night']['temp'] + progress*color_delta
+            if new_color > self.colors['day']['temp']:
+                new_color = self.colors['day']['temp']
+        elif state == 'sunset':
+            color_delta = self.colors['day']['temp'] - self.colors['night']['temp']
+            new_color = self.colors['day']['temp'] - progress*color_delta
+            if new_color < self.colors['night']['temp']:
+                new_color = self.colors['night']['temp']
+        elif state == 'day':
+            new_color = self.colors['day']['temp']
+        elif state == 'night':
+            new_color = self.colors['night']['temp']
+
+        return new_color
+
     def update_color(self, kwargs):
-        self.get_start_time()
-        self.perc_complete    = min(1, (self.current_time - self.start_time)/self.segment_length)
+        
+        now = dt.datetime.now()
+        color_state = self.determine_color_state(now)
+        progress = self.calculate_progress(now, color_state)
+        new_color = self.calculate_color(color_state, progress)
+        mired = int(1e6 / new_color)
+        brightness = 254
+
+        if DEBUG:
+            self.log("Now={}: Sunrise={}, Sunset={}, State={}, Progress={}, Color={}".format(
+                now.time(), self.sunrise().time(), self.sunset().time(),  color_state, progress, new_color))
 
         for light in self.split_device_list(self.args["light"]):
-            if DEBUG:
-                self.log("Light {} state: {}".format(light, self.get_state(entity=light, attribute='color_temp')))
-            if self.get_state(light) == "on":
-                new_color = self.color_dict[self.segment] + self.perc_complete*self.offset
-
-                # Clamp color
-                if new_color > self.daytime_color:
-                    new_color = self.daytime_color
-                elif new_color < self.nighttime_color:
-                    new_color = self.nighttime_color
-                elif (self.segment == 'twlight_night' or self.segment == 'twlight_day') and self.perc_complete > 0.99:
-                    new_color = self.twilight_color
-                
-                mired = int(1e6/new_color)
-                self.turn_on(light, color_temp=mired, brightness=self.brightness)
-                if DEBUG:
-                    self.log("Setting Light {} to {}".format(light, mired))
- 
-                if DEBUG:
-                    self.log("Current Time is {} in {} cycle and it is {}% Complete --> {} ({})".format(self.current_time, self.segment, self.perc_complete*100, mired, light))
-            
-    def get_start_time(self):
-        self.current_time = self.datetime()
-
-        if self.sun_up():
-            # sun is up, we must be in state 2 or 3
-            if self.current_time >= (self.sunset() + datetime.timedelta(minutes=self.sun_offset_mins[2])):
-                # Must be in state 3
-                self.segment         = 'twilight_night'
-                self.offset          = self.twilight_color - self.daytime_color
-                self.start_time      = self.sunset() + datetime.timedelta(minutes=self.sun_offset_mins[2])
-                self.segment_length  = datetime.timedelta(minutes=abs(self.sun_offset_mins[2]))
-            else:
-                # We are in state 2
-                self.segment         = 'daytime'
-                self.offset          = self.daytime_color - self.twilight_color
-                self.start_time      = self.sunrise() - datetime.timedelta(days=1)
-                self.segment_length  = datetime.timedelta(minutes=abs(self.sun_offset_mins[1]))
-        else:
-            if self.current_time >= (self.sunrise() + datetime.timedelta(minutes=self.sun_offset_mins[0])):
-                # Must be state 1
-                self.segment         = 'twilight_day'
-                self.offset          = self.twilight_color - self.nighttime_color
-                self.start_time      = self.sunrise() + datetime.timedelta(minutes=self.sun_offset_mins[0])
-                self.segment_length  = datetime.timedelta(minutes=abs(self.sun_offset_mins[0]))
-            else:
-                # Must be state 4
-                self.segment         = 'nighttime'
-                self.start_time      = self.sunset() - datetime.timedelta(days=1)
-                self.offset          = self.nighttime_color - self.twilight_color
-                self.segment_length  = datetime.timedelta(minutes=abs(self.sun_offset_mins[3]))
-
+            if self.get_state(light) == 'on':
+                self.turn_on(light, color_temp=mired, brightness=brightness)
