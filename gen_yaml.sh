fbebca632e39f20b5ea0d4bf86168aedcef0ed80
Kevin Fronczak <kfronczak@gmail.com>
2019-01-15 19:07:46 -0500

    RMA'd blink camera, so entity name changed

--- gen_yaml.sh
+++ gen_yaml.sh
@@ -1,2 +1,2 @@
-docker exec -it home-assistant /bin/bash -c "cd /config;bash bin/smash.sh;python lovelace/lovelace-gen.py;exit"
+docker exec -it home-assistant /bin/bash -c "cd /config;bash bin/smash.sh;exit"
 echo "Done"
